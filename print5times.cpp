1e00634ffb177b4cfb25917f73479573e1a9b7e4
Deepanshu <97582830+DeepanshuDaksh77@users.noreply.github.com>
2022-06-29 18:47:28 +0530

    Add files via upload

---
+++ print5times.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    int n = 5;
+
+    for (int i=1; i <= n; i++) {
+        std::cout<<"I am learning c++"<<std::endl;
+    }
+    return 0;
+}
---
+++ sumofNnumbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int n = 0;
+    std::cout<<"Enter the given number:";
+    std::cin>>n;
+    
+    int sum = n*(n+1)/2;
+    std::cout<<"the sum of the given number is:"<<""<<sum;
+
+    return 0;
+}
